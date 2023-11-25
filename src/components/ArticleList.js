0fedfa336a20e46841cfb8bd65c92e0bbf9d9b47
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-11-25 12:37:29 -0800

    adds Header, About,ArticleList, Article components to App.js

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Article from './Article'
+
+const ArticleList = ({posts}) => {
+    const articleArray= posts.map((post)=>{ return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}  />
+    })
+  return (
+    <main>{articleArray}</main>
+  )
+}
+
+export default ArticleList
